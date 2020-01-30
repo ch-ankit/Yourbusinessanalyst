979d9f6b202a677b357b7524e3ae685e191e3e80
Ankit Khatiwada <akgunner6@gmail.com>
2020-01-30 23:45:37 +0545

    Buyer and Stocks collection connected

--- controller/homeController.js
+++ controller/homeController.js
@@ -2,41 +2,39 @@ const moment = require('moment');
 const Stocks = require('./../models/stockModel');
 const User = require('./../models/userModel');
 
-
-
 exports.ghmpage = async (req, res) => {
-	const user = await User.findOne({ id: req.user.id });
-	let stock = {};
-	let estimatedProfit = {};
-	const docs = await Stocks.find({ userId: req.user.id }, '-_id ');
-	try {
-		docs.forEach(row => {
-
-			let date = row.Date.getMonth() + row.Date.getYear();
-			// console.log(date)
-			if (!stock.hasOwnProperty(date)) {
-				stock[date] = row.Costprice * row.Quantity;
-				estimatedProfit[date] = (row.Sellingprice - row.Costprice) * row.Quantity;
-				if (stock.hasOwnProperty((parseInt(date) - 1).toString())) {
-					stock[date] += stock[(parseInt(date) - 1).toString()]
-					estimatedProfit[date] += estimatedProfit[(parseInt(date) - 1).toString()]
-				}
-			} else {
-				stock[date] += row.Costprice * row.Quantity;
-				estimatedProfit[date] += (row.Sellingprice - row.Costprice) * row.Quantity;
-			}
-
-		});
-		res.render('index', {
-			title: 'Homepage',
-			admin: user.username,
-			accessTime: moment().format(),
-			src: './../images/smiley.jpg',
-			data: Object.keys(stock).map(el => stock[el]),
-			data1: Object.keys(estimatedProfit).map(el => estimatedProfit[el])
-		});
-	} catch (err) {
-		res.send(err)
-	}
-
+  const user = await User.findOne({ id: req.user.id });
+  let stock = {};
+  let estimatedProfit = {};
+  const docs = await Stocks.find({ userId: req.user.id }, '-_id ');
+  try {
+    docs.forEach(row => {
+      let date = row.Date.getMonth() + row.Date.getYear();
+      // console.log(date)
+      if (!stock.hasOwnProperty(date)) {
+        stock[date] = row.Costprice * row.Quantity;
+        estimatedProfit[date] =
+          (row.Sellingprice - row.Costprice) * row.Quantity;
+        if (stock.hasOwnProperty((parseInt(date) - 1).toString())) {
+          stock[date] += stock[(parseInt(date) - 1).toString()];
+          estimatedProfit[date] +=
+            estimatedProfit[(parseInt(date) - 1).toString()];
+        }
+      } else {
+        stock[date] += row.Costprice * row.Quantity;
+        estimatedProfit[date] +=
+          (row.Sellingprice - row.Costprice) * row.Quantity;
+      }
+    });
+    res.render('index', {
+      title: 'Homepage',
+      admin: user.username,
+      accessTime: moment().format(),
+      src: './../images/smiley.jpg',
+      data: Object.keys(stock).map(el => stock[el]),
+      data1: Object.keys(estimatedProfit).map(el => estimatedProfit[el])
+    });
+  } catch (err) {
+    res.send(err);
+  }
 };
--- controller/stocksController.js
+++ controller/stocksController.js
@@ -2,58 +2,87 @@ const moment = require('moment');
 const Stocks = require('./../models/stockModel');
 
 const User = require('./../models/userModel');
+const { Supplier } = require('./../models/buyerSupplierModel');
 
 exports.gpage = async (req, res, next) => {
-  const user = await User.findOne({ id: req.user.id });
-  Stocks.find(
-    { userId: req.user.id },
-    'Modelno Quantity Sellingprice Costprice -_id',
-    (err, docs) => {
-      if (!err) {
-        res.render('stocks', {
-          title: 'Stocks',
-          admin: user.username,
-          accessTime: moment().format(),
-          stocks: docs,
-          src: './../images/smiley.jpg'
-        });
-      } else {
-        next(err);
-      }
-    }
-  );
+	const user = await User.findOne({ id: req.user.id });
+	Stocks.find(
+		{ userId: req.user.id },
+		'Modelno Quantity Sellingprice Costprice -_id',
+		(err, docs) => {
+			if (!err) {
+				res.render('stocks', {
+					title: 'Stocks',
+					admin: user.username,
+					accessTime: moment().format(),
+					stocks: docs,
+					src: './../images/smiley.jpg'
+				});
+			} else {
+				next(err);
+			}
+		}
+	);
 };
 exports.addStocks = async (req, res, next) => {
-  try {
-    await Stocks.create({
-      Modelno: req.body.Modelno,
-      Sellingprice: req.body.Sellingprice,
-      Costprice: req.body.Costprice,
-      Quantity: req.body.Quantity,
-      userId: req.user.id
-    });
-    res.redirect('/stocks');
-  } catch (err) {
-    next(err);
-  }
+	try {
+		let stock = await Stocks.findOneAndUpdate(
+			{ Modelno: req.body.Modelno, userId: req.user.id },
+			{
+				$inc: {
+					Quantity: parseInt(req.body.Quantity)
+				},
+				Costprice: parseInt(req.body.Costprice),
+				Sellingprice: parseInt(req.body.Sellingprice),
+				supplierPan: req.body.supplierPannumber,
+				Modelno: req.body.Modelno,
+				userId: req.user.id
+			},
+			{ upsert: true, new: true });
+
+		let supplier = await Supplier.findOne({ userId: req.user.id, supplierPan: req.body.supplierPannumber });
+		let exist = supplier.supplies.includes(stock._id);
+		if (!exist) {
+			supplier = await Supplier.updateOne(
+				supplier,
+				{
+					userId: req.user.id,
+					supplierPan: req.body.supplierPannumber,
+					$push: {
+						supplies: stock
+					}
+				});
+		}
+
+
+		res.json(supplier);
+		// res.redirect('/stocks');
+		// }
+	} catch (err) {
+		next(err);
+	}
 };
 
-
 exports.updateQuantity = async (req, res) => {
-  try {
-    const q = await Stocks.findOne(
-      {
-        Modelno: req.body.Modelno,
-        userId: req.user.id
-      })
-    const docs = await Stocks.findOneAndUpdate({
-      Modelno: req.body.Modelno,
-      userId: req.user.id
-    }, {
-      Quantity: (q.Quantity - req.body.Quantity) < 0 ? 0 : q.Quantity - req.body.Quantity
-    });
-    res.redirect('/home');
-  } catch (err) {
-    res.send(`Error:${err}`);
-  }
+	try {
+		const q = await Stocks.findOne({
+			Modelno: req.body.Modelno,
+			userId: req.user.id
+		});
+		const docs = await Stocks.findOneAndUpdate(
+			{
+				Modelno: req.body.Modelno,
+				userId: req.user.id
+			},
+			{
+				Quantity:
+					q.Quantity - req.body.Quantity < 0
+						? 0
+						: q.Quantity - req.body.Quantity
+			}
+		);
+		res.redirect('/home');
+	} catch (err) {
+		res.send(`Error:${err}`);
+	}
 };
